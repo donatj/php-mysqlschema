ad9f2b226dc456a6472e6c7b2f2bbb66e4c8abc4
Jesse Donat <donatj@gmail.com>
2015-04-27 09:57:00 -0500

    Adds CharColumn

--- src/Columns/String/Character/CharColumn.php
+++ src/Columns/String/Character/CharColumn.php
@@ -1,9 +1,14 @@
 <?php
 
-
 namespace donatj\MySqlSchema\Columns\String\Character;
 
+use donatj\MySqlSchema\Columns\String\AbstractCharacterColumn;
 
-class CharColumn {
-
+class CharColumn extends AbstractCharacterColumn {
+	/**
+	 * @return string
+	 */
+	public function getTypeName() {
+		return 'char';
+	}
 }
