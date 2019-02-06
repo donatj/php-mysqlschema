6ffb47db629d2b1f3b34ca8f39a1092cef78416c
Jesse Donat <donatj@gmail.com>
2019-02-06 14:46:55 -0600

    Add missing implementation

--- src/Columns/String/AbstractEnumeratedColumn.php
+++ src/Columns/String/AbstractEnumeratedColumn.php
@@ -4,10 +4,12 @@ namespace donatj\MySqlSchema\Columns\String;
 
 use donatj\MySqlSchema\Columns\AbstractColumn;
 use donatj\MySqlSchema\Columns\Interfaces\CharsetColumnInterface;
+use donatj\MySqlSchema\Columns\Interfaces\EnumeratedColumnInterface;
+use donatj\MySqlSchema\Columns\Traits\EnumerationTrait;
 use donatj\MySqlSchema\Traits\CharsetAndCollationTrait;
 
-abstract class AbstractEnumeratedColumn extends AbstractColumn implements CharsetColumnInterface {
+abstract class AbstractEnumeratedColumn extends AbstractColumn implements CharsetColumnInterface, EnumeratedColumnInterface {
 
-
+	use EnumerationTrait;
 	use CharsetAndCollationTrait;
 }
